1083627c42c3a1e6886136e9cb1f2c43af231850
dsgrau <dario.grau@gmail.com>
2014-08-12 14:33:25 -0300

    Step model fixed.

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,6 +3,10 @@ class Step < ActiveRecord::Base
   belongs_to :person
   belongs_to :office
 
+  before_save :find_email
+
+  attr_accessor :person_email
+
   def first_step
     self.office = Office.start_office
     save
@@ -12,6 +16,11 @@ class Step < ActiveRecord::Base
     step.create(office: Office.archivate_office)
   end
 
+  private
+
+    def find_email
+      self.person = Person.find_or_create_by(email: person_email)
+    end
 
 
 end
