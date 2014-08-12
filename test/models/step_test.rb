1f0ae1b6156fc831f116433da81f909cd49ddbba
dsgrau <dario.grau@gmail.com>
2014-08-12 14:39:34 -0300

    Test crear step.

--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class StepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'creando step' do
+    step = Step.create(person_email: 'person@mail.com')
+    assert step.person.email == 'person@mail.com'
+  end
+
+
 end
