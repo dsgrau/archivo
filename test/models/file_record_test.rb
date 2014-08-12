95031ff0715c333318aa1fbc9db18cb02a73192c
dsgrau <dario.grau@gmail.com>
2014-08-12 14:40:28 -0300

    Test creando file record.

--- test/models/file_record_test.rb
+++ test/models/file_record_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class FileRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Cuando creamos file record' do
+    file_record = FileRecord.create
+    assert file_record.steps.count == 1
+  end
+
 end
