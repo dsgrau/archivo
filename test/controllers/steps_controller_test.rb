6b11a466d4b8c4007e2722dbbea2d8076e6beb74
dsgrau <dario.grau@gmail.com>
2014-08-14 20:17:03 -0300

    Nuevos test step.

--- test/controllers/steps_controller_test.rb
+++ test/controllers/steps_controller_test.rb
@@ -10,4 +10,20 @@ class StepsControllerTest < ActionController::TestCase
     assert_redirected_to edit_person_path(step.person)
   end
 
+  #test "step cuando la persona esta completa" do
+  #  step_hash = { person: people(:completo), file_record: file_records(:one)}
+  #  step = Step.new(step_hash)
+  #  post :create,  step: step_hash 
+  #  assert_redirected_to file_record_path(step.file_record)
+  #end
+
+  test "cuando la persona incompleta" do
+        
+    post :create, step: { person_email: 'incompleto@mail.com' , file_record: file_records(:one)}
+    person = Person.find_by(email: 'incompleto@mail.com')
+    assert_redirected_to edit_person_path(person)
+
+  end
+
+
 end
